0eaa65df9672a5a24f549331b9e1d0bab77fcf5a
InternetMadeCoder <anonymous210705@gmail.com>
2025-04-13 18:21:25 +0530

    fixed navigation

--- src/components/about-conference/AboutConference.jsx
+++ src/components/about-conference/AboutConference.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./Hero";
 import WelcomeSection from "./WelcomeSection";
 import ObjectivesSection from "./ObjectivesSection";
 import { KeynoteSpeakers } from "../home/KeynoteSpeakers";
 
 const AboutConference = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <main className="min-h-screen">
       <Hero />
