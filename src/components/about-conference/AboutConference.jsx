cd1c9a84227c300c54349dcdd628f34b8a287a46
InternetMadeCoder <anonymous210705@gmail.com>
2025-04-13 18:15:49 +0530

    keynote speakers in about conference page

--- src/components/about-conference/AboutConference.jsx
+++ src/components/about-conference/AboutConference.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import WelcomeSection from "./WelcomeSection";
 import ObjectivesSection from "./ObjectivesSection";
+import { KeynoteSpeakers } from "../home/KeynoteSpeakers";
 
 const AboutConference = () => {
   return (
@@ -9,6 +10,9 @@ const AboutConference = () => {
       <Hero />
       <WelcomeSection />
       <ObjectivesSection />
+      <div className="bg-white">
+        <KeynoteSpeakers />
+      </div>
     </main>
   );
 };
